1aa859bb22135905ddc034404f669d893151c854
jaydenSalazar <75498297+jaydenSalazar@users.noreply.github.com>
2021-06-04 13:05:32 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+ 
+
+$(".submit").click(function() {
+    let input= $('.name').val();
+    let intro= $('.MovieNumber').val();
+    let answer1= $(".HeightsQ").val(); 
+    let answer2 = $(".SacrificeQ").val();
+    let answer3 = $(".stonesQ").val();
+    let answer4= $(".HulkQ").val();
+    let answer5 = $(".CaptainQ").val();
+   
+ 
+});
+
+
+
+
+ 
+
